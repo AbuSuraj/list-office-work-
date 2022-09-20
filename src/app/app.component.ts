a1ef43b5e54ff8251242f917e1bc190fcca95140
abu bokor suraj <abusurajbd@gmail.com>
2022-09-20 12:02:45 +0600

    done making list of programming languages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import name from './files/name.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'list';
-  programmingLanguages = ['C','C++','C#', 'pytthon','java', 'javaScript','R', "PHP" ]
+// programmingLanguages = ['C','C++','C#', 'pytthon','java', 'javaScript','R', "PHP" ]
+// url = 'https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata';
+
+export class AppComponent  {
+	title = "List";
+  public programmingLanguages: {name: string, code:string}[]= name;
+	
 }
